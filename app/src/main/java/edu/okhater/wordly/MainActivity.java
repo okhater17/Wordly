404115ed88a7a9db302c7d3d1186e117bc927a7d
Noah F <noahfox555@gmail.com>
2023-03-07 13:31:44 -0500

    splash screen

--- app/src/main/java/edu/okhater/wordly/MainActivity.java
+++ app/src/main/java/edu/okhater/wordly/MainActivity.java
@@ -1,37 +1,45 @@
 package edu.okhater.wordly;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-    BufferedReader reader;
-    Graph gr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            reader = new BufferedReader(
-                    new InputStreamReader(getAssets().open("words_test.txt")));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        View rootView = findViewById(R.id.root_cl);
+
+        rootView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
         }
-        try {
-            gr = new Graph(reader);
-            //Log.d("CPS", gr.g.get(i).name + "\n" + gr.g.get(i).successors.toString());
-            for(int i = 0; i < 10; i++) {
-                Log.d("CPS", gr.findRandomWordsPath().toString());
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), StartActivity.class);
+                startActivity(i);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        }, 1375);
     }
 }
