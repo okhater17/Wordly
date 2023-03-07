594c98e159e9b67b70413ade57c76bd870abcee9
omar <okhater@fandm.edu>
2023-03-07 14:01:53 -0500

    Finished StartActivity, added GameActivity

--- app/src/main/java/edu/okhater/wordly/StartActivity.java
+++ app/src/main/java/edu/okhater/wordly/StartActivity.java
@@ -2,15 +2,16 @@ package edu.okhater.wordly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
+
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,14 +19,26 @@ import java.util.concurrent.Executors;
 public class StartActivity extends AppCompatActivity {
     BufferedReader reader;
     Graph gr;
-    ArrayList<String> path;
     interface GraphCallBack{
         void onComplete(ArrayList<String> s);
     }
     GraphCallBack gcb = new GraphCallBack() {
         @Override
         public void onComplete(ArrayList<String> s) {
-            path = s;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(s != null) {
+                        Intent i = new Intent(getApplicationContext(), GameActivity.class);
+                        i.putExtra("path", s);
+                        startActivity(i);
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Words not supported!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
         }
     };
 
@@ -35,7 +48,8 @@ public class StartActivity extends AppCompatActivity {
             es.execute(new Runnable() {
                 @Override
                 public void run() {
-                    gcb.onComplete(gr.findPath(word1, word2));
+                    ArrayList<String>s = gr.findPath(word1, word2);
+                    gcb.onComplete(s);
                 }
             });
         }
@@ -44,7 +58,8 @@ public class StartActivity extends AppCompatActivity {
             es.execute(new Runnable() {
                 @Override
                 public void run() {
-                    gcb.onComplete(gr.findRandomWordsPath());
+                    ArrayList<String> s = gr.findRandomWordsPath();
+                    gcb.onComplete(s);
                 }
             });
         }
@@ -64,24 +79,18 @@ public class StartActivity extends AppCompatActivity {
         findViewById(R.id.play).setOnClickListener(view -> {
             EditText word1 = findViewById(R.id.word1);
             EditText word2 = findViewById(R.id.word2);
-            String word1Str = word1.toString();
-            String word2Str = word2.toString();
-            if(gr.find(word1Str) == null || gr.find(word2Str) == null){
-                Toast.makeText(getApplicationContext(), "Words are not supported! Sorry!", Toast.LENGTH_SHORT).show();
-                return;
-            }
+            String word1Str = word1.getText().toString();
+            String word2Str = word2.getText().toString();
             if(word1Str.length() != word2Str.length()){
                 Toast.makeText(getApplicationContext(), "Enter two words of the same length!", Toast.LENGTH_SHORT).show();
             }
             else{
                 new GraphExecutor().findPath(gcb, word1Str, word2Str);
-                //Send them to the game
             }
         });
 
         findViewById(R.id.random).setOnClickListener(view -> {
             new GraphExecutor().findRandomPath(gcb);
-            //Send them to the game
         });
 
     }
