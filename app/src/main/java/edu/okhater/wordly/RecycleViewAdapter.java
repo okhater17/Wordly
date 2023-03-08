67364fa1c408c88e295d93f11705c4a9283c1933
omar <okhater@fandm.edu>
2023-03-08 01:17:01 -0500

    Removed some imports

--- app/src/main/java/edu/okhater/wordly/RecycleViewAdapter.java
+++ app/src/main/java/edu/okhater/wordly/RecycleViewAdapter.java
@@ -1,13 +1,9 @@
 package edu.okhater.wordly;
-
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
